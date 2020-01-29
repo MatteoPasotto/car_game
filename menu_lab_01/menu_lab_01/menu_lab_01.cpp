164ac98c724d5f85dc3838f89b981be7eaa92066
MatteoPasotto <44723104+MatteoPasotto@users.noreply.github.com>
2020-01-29 09:39:33 +0100

    Add files via upload

---
+++ menu_lab_01/menu_lab_01/menu_lab_01.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+#include "controlli.h"
+
+
+void crea_mappa() {
+	char mappa[30][30];
+	int i, j;
+	for (i = 0; i < 30; i++)
+	{
+		for (j = 0; j < 30; j++) {
+			mappa[i][j] = ' ';
+
+		}
+
+	}
+	for (i = 0; i < 30; i++)
+	{
+		for (j = 0; j < 30; j++) {
+			if (j == 0 || j == 29) {
+				mappa[i][j] = 'X';
+			}
+
+		}
+
+	}
+
+	for (i = 0; i < 30; i++)
+	{
+		for (j = 0; j < 30; j++) {
+			
+				cout << mappa[i][j];
+			
+
+		}
+		cout << endl << ;
+	}
+}
+
+int main()
+{
+	int x;
+	bool flag;
+	do {
+		system("cls");
+		cout << " Fast2Furious " << endl;
+		cout << "1-Gioca" << endl;
+		cout << "2-Istruzioni" << endl;
+		cout << "3-Best Score" << endl;
+		cout << "4-titoli di coda" << endl;
+		cout << "5-esci" << endl;
+		cin >> x;
+
+		flag = NumCompreso(x, 1, 5); //controlla che il valore di x sia compreso tra min e max restitutisce true se il numero è compreso  
+
+	} while (flag == false);
+	cin.clear();
+	if (x == 1) {
+		crea_mappa();
+	 }
+	return 0;
+}
+
